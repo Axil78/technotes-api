b3f6cc1f517d1f45ed53f4d53e5b16829ff7c67a
Axil78 <142527230+Axil78@users.noreply.github.com>
2024-11-20 12:58:01 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/authController')
-const loginLimiter = require('../middleware/loginLimiter')
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const loginLimiter = require('../middleware/loginLimiter');
 
+// Login route
 router.route('/')
-    .post(loginLimiter,
-        authController.login)
+    .post(
+        loginLimiter,         // Rate limiter middleware
+        authController.login  // Login controller
+    );
 
+// Refresh token route
 router.route('/refresh')
-    .get(authController.refresh)
+    .get(authController.refresh);
 
+// Logout route
 router.route('/logout')
-    .post(authController.logout)
+    .post(authController.logout);
 
-module.exports = router
+module.exports = router;
